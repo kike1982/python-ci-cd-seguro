a69d13eae746fc8651c1a271a2b0ff1ac234a71f
kike1982 <kike82andrea@gmail.com>
2026-01-05 12:27:28 +0100

    Update test_main.py

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,4 +1,9 @@
+import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 from app.main import suma
 
 def test_suma():
     assert suma(2, 2) == 4
+
